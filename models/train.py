80c28c1e823d720f30bcb4bd19e7f70d6f01a676
ShivaniiB <shivaniibalu@gmail.com>
2025-10-27 02:50:36 +0000

    transdorm data

--- models/train.py
+++ models/train.py
@@ -6,3 +6,13 @@ From tensorflow.keras.models import Sequential
 From tensorflow.keras.layers import Dense
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.models import load_model
+
+df = pd.read_csv('data/customer_churn.csv')
+x = df.drop(columns=['customerID', 'Surname','Exited'], axis=1)
+y = df['Exited']
+x['gender'] = label_encoder.fit_transform(x['gender'])
+x['Geography'] = label_encoder.fit_transform(x['Geography'])
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
